8bf297a7c59c720fa7771f12cfc44372fbbc203b
Gustavobiz <gustavobernardes05@hotmail.com>
2024-04-20 22:11:17 -0300

    materia controlle basico terminado

--- sge/src/main/java/br/imd/ufrn/sge/controller/MateriaController.java
+++ sge/src/main/java/br/imd/ufrn/sge/controller/MateriaController.java
@@ -1,16 +1,55 @@
 package br.imd.ufrn.sge.controller;
 
+import br.imd.ufrn.sge.models.DadosPessoais;
 import br.imd.ufrn.sge.models.materia.Materia;
-import br.imd.ufrn.sge.service.DadosPessoaisService;
+import br.imd.ufrn.sge.relatorio.repository.MateriaRepository;
+import br.imd.ufrn.sge.service.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/api/materias", produces="application/json")
 public class MateriaController {
 
     @Autowired
-    private Materia materia;
+    private MateriaService materiaService;
+
+    @GetMapping
+    public List<Materia> listarMaterias() {
+        return materiaService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> obterMateriaPorId(@PathVariable Long id) {
+        Optional<Materia> materia= materiaService.encontrarPorId(id);
+
+        if (materia.isPresent()){
+            return ResponseEntity.ok().body(materia.get());
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Materia com o ID " + id + " não encontrada");
+        }
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<Materia> criarMateria(@RequestBody Materia materia) {
+        Materia novaMateria = materiaService.salvar(materia);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaMateria);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarMateria(@PathVariable Long id) {
+        Optional<Materia> materiaExistente = materiaService.encontrarPorId(id);
+        if (materiaExistente.isPresent()) {
+            materiaService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
--- sge/src/main/java/br/imd/ufrn/sge/service/MateriaService.java
+++ sge/src/main/java/br/imd/ufrn/sge/service/MateriaService.java
@@ -1,11 +1,14 @@
 package br.imd.ufrn.sge.service;
 
+import br.imd.ufrn.sge.models.DadosPessoais;
 import br.imd.ufrn.sge.models.materia.Materia;
 import br.imd.ufrn.sge.relatorio.repository.MateriaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MateriaService {
@@ -13,10 +16,20 @@ public class MateriaService {
     @Autowired
     private MateriaRepository materiaRepository ;
 
-    public List<Materia> findByName(String nome_materia) {
-        return materiaRepository.findByName(nome_materia);
+    public List<Materia> listarTodos() {return materiaRepository.findAll();}
+
+    public Optional<Materia> encontrarPorId(Long id) {
+        return materiaRepository.findById(id);
     }
-    public List<Materia> findById(String id_materia) {
-        return materiaRepository.findByName(id_materia);
+
+    @Transactional
+    public Materia salvar(Materia materia) {
+        return materiaRepository.save(materia);
     }
+
+    @Transactional
+    public void deletar(Long id) {
+        materiaRepository.deleteById(id);
+    }
+
 }
