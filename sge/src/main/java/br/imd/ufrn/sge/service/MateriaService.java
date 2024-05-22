4b4999a3b7e919435c8d8953ceb1100eafea333f
Gustavobiz <gustavobernardes05@hotmail.com>
2024-05-22 20:57:43 -0300

    cria uma materia e liga ao docente por meio do id

--- sge/src/main/java/br/imd/ufrn/sge/service/MateriaService.java
+++ sge/src/main/java/br/imd/ufrn/sge/service/MateriaService.java
@@ -1,6 +1,8 @@
 package br.imd.ufrn.sge.service;
 
+import br.imd.ufrn.sge.exceptions.IdNaoEncontradoException;
 import br.imd.ufrn.sge.models.materia.Materia;
+import br.imd.ufrn.sge.repository.DocenteRepository;
 import br.imd.ufrn.sge.repository.MateriaRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +17,27 @@ public class MateriaService {
     @Autowired
     private MateriaRepository materiaRepository ;
 
+    @Autowired
+    private DocenteRepository docenteRepository;
+
     public List<Materia> listarTodos() {return materiaRepository.findAll();}
 
     public Optional<Materia> encontrarPorId(Long id) {
-        return materiaRepository.findById(id);
+        if (materiaRepository .findById(id).isEmpty()){
+            throw new IdNaoEncontradoException();
+        }
+        return materiaRepository .findById(id);
     }
+
 
     @Transactional
     public Materia salvar(Materia materia) {
+        return materiaRepository.save(materia);
+    }
+
+    @Transactional
+    public Materia salvar(Long id,Materia materia) {
+        materia.setDocente(docenteRepository.findById(id).get());
         return materiaRepository.save(materia);
     }
 
