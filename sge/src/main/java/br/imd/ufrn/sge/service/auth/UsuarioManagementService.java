a5f1404f00e5f082cae88fdda238a7b88504a670
luiz.henrique <lhtargino@gmail.com>
2024-05-22 02:21:37 -0300

    feat: Controller de autenticação.

--- sge/src/main/java/br/imd/ufrn/sge/service/auth/UsuarioManagementService.java
+++ sge/src/main/java/br/imd/ufrn/sge/service/auth/UsuarioManagementService.java
@@ -85,4 +85,52 @@ public class UsuarioManagementService {
         }
         return userReqResponseDTO;
     }
+
+    // Refresca o token
+    public UserReqResponseDTO refresh(UserReqResponseDTO userReqResponseDTO) throws AuthException {
+        UserReqResponseDTO userReqResponseDTO1 = new UserReqResponseDTO();
+        Optional<Usuario> user = userService.findByLogin(userReqResponseDTO.getUser().getLogin());
+        if (user.isPresent()){
+            String token = jwtService.generateToken(user.get());
+            String refreshToken = jwtService.refreshToken(new HashMap<>(),user.get());
+            userReqResponseDTO1.setToken(token);
+            userReqResponseDTO1.setRefreshToken(refreshToken);
+            userReqResponseDTO1.setExpirationTime("15 minutos");
+            userReqResponseDTO1.setMessage("Token atualizado com sucesso");
+            userReqResponseDTO1.setStatusCode(200);
+        }else {
+            throw new AuthException("Usuário não encontrado");
+        }
+        return userReqResponseDTO1;
+    }
+
+    public UserReqResponseDTO getMyInfo(String login) throws AuthException {
+        UserReqResponseDTO userReqResponseDTO = new UserReqResponseDTO();
+        Optional<Usuario> user = userService.findByLogin(login);
+        if (user.isPresent()){
+            userReqResponseDTO.setUser(user.get());
+            userReqResponseDTO.setStatusCode(200);
+        }else {
+            throw new AuthException("Usuário não encontrado");
+        }
+        return userReqResponseDTO;
+    }
+
+    public UserReqResponseDTO updateUser(UserReqResponseDTO userReqResponseDTO) throws AuthException {
+        UserReqResponseDTO userReqResponseDTO1 = new UserReqResponseDTO();
+        Optional<Usuario> user = userService.findByLogin(userReqResponseDTO.getUser().getLogin());
+        if (user.isPresent()){
+            user.get().setLogin(userReqResponseDTO.getUser().getLogin());
+            user.get().setRole(userReqResponseDTO.getUser().getRole());
+            user.get().setSenha(passwordEncoder.encode(userReqResponseDTO.getUser().getPassword()));
+            Usuario usuario = userService.save(user.get());
+            if (usuario.getId() >0 ){
+                userReqResponseDTO1.setMessage("Usuário atualizado com sucesso");
+                userReqResponseDTO1.setStatusCode(200);
+            }
+        }else {
+            throw new AuthException("Usuário não encontrado");
+        }
+        return userReqResponseDTO1;
+    }
 }
